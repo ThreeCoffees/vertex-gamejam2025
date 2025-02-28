187132b26b09aa2c35740526abc61c8b2e56849c
Adam Matczak <amatczak2k5@gmail.com>
2025-02-28 23:26:19 +0100

    rebase

--- Assets/Scripts/ItemController.cs
+++ Assets/Scripts/ItemController.cs
@@ -4,10 +4,15 @@ using UnityEngine;
 
 public class ItemController : MonoBehaviour
 {
+
+    enum ItemType{
+        Hammer,
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
