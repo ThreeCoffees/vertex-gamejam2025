04864c11cf0548f4e82b048ab0d50c9d1bf824fa
ThreeCoffes <leonard.madzielewski@gmail.com>
2025-03-01 15:56:51 +0100

    better bg image

--- Assets/Scripts/DeviceController.cs
+++ Assets/Scripts/DeviceController.cs
@@ -30,7 +30,7 @@ public class DeviceController : MonoBehaviour
     }
 
     public void updateRepairIcons(){
-        iconsSize.sizeDelta = new Vector2(32 * requiredItems.Count, 32);
+        iconsSize.sizeDelta = new Vector2(42 + (32 * (requiredItems.Count - 1)), 42);
         foreach(Transform child in repairIcons.transform){
             GameObject.Destroy(child.gameObject);
         }
