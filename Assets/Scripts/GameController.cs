b5ebcdafa2b912540769fbb2d02aa1a62de853dd
ThreeCoffes <leonard.madzielewski@gmail.com>
2025-03-02 17:57:58 +0100

    keybind reset

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -15,6 +15,9 @@ public class GameController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if(Input.GetKeyDown(KeyCode.R)){
+            ResetScene();
+        }
         if (reset) {
             ResetScene();
             reset = false;
