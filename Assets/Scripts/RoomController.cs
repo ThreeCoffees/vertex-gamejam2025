60a58b48ae309c9f6df8b2ff0614042eea3037f1
ThreeCoffes <leonard.madzielewski@gmail.com>
2025-03-02 13:27:39 +0100

    full ship breaking setup

--- Assets/Scripts/RoomController.cs
+++ Assets/Scripts/RoomController.cs
@@ -8,14 +8,38 @@ public class RoomController : MonoBehaviour
     [SerializeField]List<GameObject> doors;
     private Collider2D roomCollider;
 
+    bool isDestroyed = false;
+
     void Awake(){
         roomCollider = GetComponent<Collider2D>();
     }
 
     void Update()
     {
-        foreach(GameObject d in devices){
-            if((DeviceController))
+        if(isDestroyed == true){
+            return;
+        }
+
+        foreach(GameObject o in devices){
+            DeviceController d = o.GetComponent<DeviceController>();
+            if(!d.isDestroyed()){
+                return;
+            }
+        }
+
+        DestroyRoom();
+    }
+
+    void OnTriggerStay2D(Collider2D other){
+        if(other.CompareTag("Player") && isDestroyed){
+            Debug.Log("Player died");
+        }
+    }
+
+    void DestroyRoom(){
+        isDestroyed = true;
+        foreach(GameObject o in doors){
+            o.GetComponent<DoorController>().locked = true;
         }
     }
 }
